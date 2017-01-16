1af002b98151a6e6f012e9e33d9b4860a8a2c57f
Gerhard Freudenthaler <gefr@tf.uni-kiel.de>
2017-01-16 15:20:39 +0100

    - comments to current interface added

--- Sources/rte/rte.h
+++ Sources/rte/rte.h
@@ -21,24 +21,78 @@
 #define EXTERNAL_ extern
 #endif
 
+/**
+ * @brief RTE interface to turn the right LED ON
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedRiOn();
 
+/**
+ * @brief RTE interface to turn the right LED OFF
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedRiOff();
 
+/**
+ * @brief RTE interface to toggle the state the right LED
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedRiNeg();
 
+/**
+ * @brief RTE interface to write the state of the right LED
+ * @param state_ desired state of the LED
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedRiSt(uint8 state_);
 
+/**
+ * @brief RTE interface to read the state of the right LED
+ * @param *state_ output: pointer to the LED state
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Read_LedRiSt(uint8 *state_);
 
+/**
+ * @brief RTE interface to turn the left LED ON
+ * @param *state_ pointer to the LED state (call by reference)
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedLeOn();
 
+/**
+ * @brief RTE interface to turn the left LED OFF
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedLeOff();
 
+/**
+ * @brief RTE interface to toggle the state the left LED
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedLeNeg();
 
+/**
+ * @brief RTE interface to write the state of the left LED
+ * @param state_ desired state of the LED
+ * @return Error code, RET_OK if everything was fine,
+ *                     RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Write_LedLeSt(uint8 state_);
 
+/**
+ * @brief RTE interface to read the state of the right LED
+ * @param *state_ pointer to the LED state (call by reference)
+ * @return Error code, RET_OK if everything was fine, RET_INVALID otherwise
+ */
 EXTERNAL_ StdRtnType RTE_Read_LedLeSt(uint8 *state_);
 
 
