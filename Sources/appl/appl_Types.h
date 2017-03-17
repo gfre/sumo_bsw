77baa9c4a71b54592127a6be1b57fa8a7c66c47d
G Freudenthaler <gefr@tf.uni-kiel.de>
2017-03-17 14:53:26 +0100

    - function declarations commented

--- Sources/appl/appl_Types.h
+++ Sources/appl/appl_Types.h
@@ -55,16 +55,55 @@ typedef enum APPL_State_e
 
 /*============================ >> GLOBAL FUNCTION DECLARATIONS << ================================*/
 /*!
- * @brief This function returns the address of the variable of the application state
- * @return pointer to the variable of the application state
+ * @brief This function returns the next targeted state of state machine handling application software
+ * @return targeted state
  */
 EXTERNAL_ APPL_State_t APPL_Get_NextState(void);
+
+/*!
+ * @brief This function returns the current state of state machine handling application software
+ * @return current application state
+ */
 EXTERNAL_ APPL_State_t APPL_Get_SmState(void);
+
+/*!
+ * @brief This function returns the current command of state machine handling application software
+ * @return current application state
+ */
 EXTERNAL_ APPL_Cmd_t APPL_Get_SmCmd(void);
 
+/*!
+ * @brief This function sets a flag which enables/disables hold on ENTER functionality for a certain state
+ * @param state_ corresponding application state for holdOn ENTER functionality
+ * 		  holdOn_ TRUE/FALSE-flag for enabling or disabling holdOn ENTER functionality
+ * @return Error code, ERR_OK if everything was fine,
+ *                     ERR_PARAM_ADDRESS otherwise
+ */
 EXTERNAL_ StdRtn_t Set_HoldOnEnter(const APPL_State_t state_, const uint8_t holdOn_);
+
+/*!
+ * @brief This function sets a flag which enables/disables hold on EXIT functionality for a certain state
+ * @param state_ corresponding application state for holdOn EXIT functionality
+ * 		  holdOn_ TRUE/FALSE-flag for enabling or disabling holdOn EXIT functionality
+ * @return Error code, ERR_OK if everything was fine,
+ *                     ERR_PARAM_ADDRESS otherwise
+ */
 EXTERNAL_ StdRtn_t Set_HoldOnExit(const APPL_State_t state_, const uint8_t holdOn_);
+
+/*!
+ * @brief This function allows to release hold on ENTER for a certain state during runtime
+ * @param state_ corresponding application state which shall be released
+ * @return Error code, ERR_OK if everything was fine,
+ *                     ERR_PARAM_ADDRESS otherwise
+ */
 EXTERNAL_ StdRtn_t Set_ReleaseEnter(const APPL_State_t state_);
+
+/*!
+ * @brief This function allows to release hold on EXIT for a certain state during runtime
+ * @param state_ corresponding application state which shall be released
+ * @return Error code, ERR_OK if everything was fine,
+ *                     ERR_PARAM_ADDRESS otherwise
+ */
 EXTERNAL_ StdRtn_t Set_ReleaseExit(const APPL_State_t state_);
 
 #ifdef EXTERNAL_
