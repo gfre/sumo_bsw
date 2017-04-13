7bb946e8a4d482d4e2a038144a944a2b271f22e6
G Freudenthaler <gefr@tf.uni-kiel.de>
2017-04-13 10:45:06 +0200

    - doc and include fix in batt.c and buz.c

--- Sources/batt/batt.c
+++ Sources/batt/batt.c
@@ -14,7 +14,7 @@
  *
  ***************************************************************************************************/
 
-#define MASTER_batt_clshdlr_C_
+#define MASTER_batt_C_
 
 /*======================================= >> #INCLUDES << ========================================*/
 #include "ACon_Types.h"
@@ -29,6 +29,7 @@
 #define SAMPLE_GROUP_SIZE 1U
 #define BAT_V_DIVIDER_UP   62 /* voltage divider pull-up */
 #define BAT_V_DIVIDER_DOWN 30 /* voltage divider pull-down */
+
 
 
 /*=================================== >> TYPE DEFINITIONS << =====================================*/
@@ -71,6 +72,8 @@ StdRtn_t BATT_MeasureBatteryVoltage(uint16_t *cvP)
 }
 
 
+
+/*============================= >> GLOBAL FUNCTION DEFINITIONS << ================================*/
 void BATT_Init(void)
 {
 	/* nothing to do */
--- Sources/buz/buz.c
+++ Sources/buz/buz.c
@@ -3,8 +3,8 @@
  * @ingroup		buz
  * @brief 		Implementation of a driver component for a buzzer.
  *
- * This module implements driver component for a buzzer by means of trigger provided and enhanced
- * by the firmware components @b TRG1 and @b BUZ1.
+ * This module implements a driver component for a buzzer by means of a trigger component provided
+ * and enhanced by the firmware components @b TRG1 and @b BUZ1.
  *
  * @author 	(c) 2014 Erich Styger, erich.styger@hslu.ch, Hochschule Luzern
  * @author 	G. Freudenthaler, gefr@tf.uni-kiel.de, Chair of Automatic Control, University Kiel
@@ -14,6 +14,9 @@
  *
  ***************************************************************************************************/
 
+#define MASTER_buz_C_
+
+/*======================================= >> #INCLUDES << ========================================*/
 #include "Platform.h"
 #include "buz.h"
 #include "buz_cfg.h"
@@ -22,12 +25,30 @@
 #include "TRG1.h"
 
 
+
+/*======================================= >> #DEFINES << =========================================*/
 #define TRG_TICKS_MS  1
 
+
+/*=================================== >> TYPE DEFINITIONS << =====================================*/
+
+
+
+/*============================= >> LOKAL FUNCTION DECLARATIONS << ================================*/
+static void BUZ_Toggle();
+static void BUZ_Play();
+
+
+/*=================================== >> GLOBAL VARIABLES << =====================================*/
 static BUZ_Tunes_t tune;
 static volatile BUZ_TrgInfo trgInfo;
 static MelodyDesc *BUZ_Melodies = NULL;
-static void BUZ_Toggle() {
+
+
+
+/*============================== >> LOKAL FUNCTION DEFINITIONS << ================================*/
+static void BUZ_Toggle()
+{
   if (trgInfo.buzIterationCntr==0) {
 	  BUZ1_ClrVal(); /* turn buzzer off */
   } else {
@@ -37,19 +58,8 @@ static void BUZ_Toggle() {
   }
 }
 
-uint8_t BUZ_Beep(uint16_t freq, uint16_t durationMs) {
-  if (trgInfo.buzIterationCntr==0) { /* only if buzzer is not running right now */
-	  BUZ1_SetVal(); /* turn buzzer on */
-	  trgInfo.buzPeriodTicks = (1000*TRG_TICKS_MS)/freq;
-	  trgInfo.buzIterationCntr = durationMs/TRG_TICKS_MS/trgInfo.buzPeriodTicks;
-	  TRG1_AddTrigger(TRG1_BUZ_BEEP, trgInfo.buzPeriodTicks, BUZ_Toggle);
-	  return ERR_OK;
-  } else {
-	  return ERR_BUSY;
-  }
-}
-
-static void BUZ_Play() {
+static void BUZ_Play()
+{
   MelodyDesc *melody = &BUZ_Melodies[tune];
 
   if (NULL != melody)
@@ -61,6 +71,21 @@ static void BUZ_Play() {
       }
   }
   return;
+}
+
+
+
+/*============================= >> GLOBAL FUNCTION DEFINITIONS << ================================*/
+uint8_t BUZ_Beep(uint16_t freq, uint16_t durationMs) {
+  if (trgInfo.buzIterationCntr==0) { /* only if buzzer is not running right now */
+	  BUZ1_SetVal(); /* turn buzzer on */
+	  trgInfo.buzPeriodTicks = (1000*TRG_TICKS_MS)/freq;
+	  trgInfo.buzIterationCntr = durationMs/TRG_TICKS_MS/trgInfo.buzPeriodTicks;
+	  TRG1_AddTrigger(TRG1_BUZ_BEEP, trgInfo.buzPeriodTicks, BUZ_Toggle);
+	  return ERR_OK;
+  } else {
+	  return ERR_BUSY;
+  }
 }
 
 uint8_t BUZ_PlayTune(BUZ_Tunes_t tune_) {
@@ -85,3 +110,8 @@ void BUZ_Init(void) {
   trgInfo.buzIterationCntr = 0;
 }
 
+
+
+#ifdef MASTER_buz_C_
+#undef MASTER_buz_C_
+#endif /* !MASTER_buz_C_ */
