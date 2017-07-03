23ac9b7898cf2e26cdd81886f4b2ec58c78488f3
simon helling <simon.helling@gmx.de>
2017-07-03 09:18:45 +0200

    -added software component string macro for task_cfg.c

--- Sources/kf/kf.h
+++ Sources/kf/kf.h
@@ -1,42 +1,35 @@
 /***********************************************************************************************//**
  * @file		kf.h
- * @ingroup		<group label>
- * @brief 		<This is a brief description.>
+ * @ingroup		kf Kalman Filter
+ * @brief 		This header file contains the type definitions for matrices and column and row vectors
  *
- * <This is a detailed description.>
  *
- * @author 	<I>. <Surname>, Simon Helling@tf.uni-kiel.de, Chair of Automatic Control, University Kiel
+ * @author 	S. Helling, stu112498@tf.uni-kiel.de, Chair of Automatic Control, University Kiel
  * @date 	23.06.2017
  *
- * @copyright @<LGPLx_x>
+ * @copyright @<LGPL2_1>
  *
  ***************************************************************************************************/
 
-#define MASTER_KF_C_
+#define KF_H_
 
 /*======================================= >> #INCLUDES << ========================================*/
 #include "Platform.h"
 #include "Acon_Types.h"
 
+#ifdef MASTER_KF_C_
+#define EXTERNAL_
+#else
+#define EXTERNAL_ extern
+#endif
+
 /*======================================= >> #DEFINES << =========================================*/
 #define KF_SYS_DIMENSION	(0x02u)
+#define KF_SWC_STRING		("kalman filter")
 
 
 
 /*=================================== >> TYPE DEFINITIONS << =====================================*/
-//typedef struct KF_I16Vec_s {
-//	int16_t aRow[KF_SYS_DIMENSION];
-//}KF_I16Vec_t;
-//
-//typedef struct KF_I16VecCol_s {
-//	int16_t aCol[KF_SYS_DIMENSION];
-//}KF_I16VecCol_t;
-//
-//typedef struct KF_I16Matrix_s {
-//	KF_I16VecCol_t aRow[KF_SYS_DIMENSION];
-////	uint8_t dim;
-//}KF_I16Mat_t;
-
 typedef struct KF_I32ColVec_s {
 	int32_t aRow[KF_SYS_DIMENSION];
 }KF_I32ColVec_t;
@@ -47,7 +40,6 @@ typedef struct KF_I32RowVec_s {
 
 typedef struct KF_I32Matrix_s {
 	KF_I32RowVec_t aRow[KF_SYS_DIMENSION];
-//	uint8_t dim;
 }KF_I32Mat_t;
 
 /*============================= >> LOKAL FUNCTION DECLARATIONS << ================================*/
@@ -61,6 +53,6 @@ void KF_Init(void);
 void KF_Main(void);
 
 
-#ifdef MASTER_KF_C_
-#undef MASTER_KF_C_
-#endif /* !MASTER_KF_C_ */
+#ifdef EXTERNAL_
+#undef EXTERNAL_
+#endif /* KF_H_ */
